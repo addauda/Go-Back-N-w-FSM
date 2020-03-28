7cedfc36ec8ae5fcb04f3e8d224b7c3b58db60d5
Ahmed Dauda <ahmed.dauda@uoit.net>
2020-03-27 21:52:37 -0500

    updated comments, playing around with default delay/loss

--- receiver.sh
+++ receiver.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ $# -gt 0 ]; then
-    echo "Sender started with $# arguments"
+    echo "Receiver started with $# arguments"
     node receiver.js $1 $2 $3 $4
 else
-    echo "Sender started with no params - using defaults"
+    echo "Receiver started with no params - using defaults"
     EMU_IP=10.0.2.15
     EMU_ACK_PORT=9993
     RCVR_PORT=9994
